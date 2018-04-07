29c450e91f687c46a039bd84c561aea97289af06
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-04-06 20:43:44 -0400

    Create PSR4Autoload.php

---
+++ autoloader/PSR4Autoload.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+/**
+ * PSR4 Autoloader.
+ *
+ * @author  Autoloader Contributors <https://github.com/genial-framework/autoloader/graphs/contributors>.
+ *
+ * @link    <https://github.com/genial-framework/autoloader> Github Repository.
+ * @license <https://github.com/genial-framework/autoloader/master/LICENSE> MIT License.
+ */
+
+namespace PSR_4_Autoloader
+
+/**
+ * AutoloaderInterface.
+ */
+class Autoloader implements AutoloaderInterface
+{
+}
