9640f7edcbb67a05d4b04e55cc78624284f138fa
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-04-06 21:35:22 -0400

    Create boot.php

---
+++ boot.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+/**
+ * FileSystem.
+ *
+ * @author  FileSystem Contributors <https://github.com/genial-framework/filesystem/graphs/contributors>.
+ *
+ * @link    <https://github.com/genial-framework/filesystem> Github Repository.
+ * @license <https://github.com/genial-framework/filesystem/master/LICENSE> MIT License.
+ */
+
+ini_set('allow_url_fopen', 0);
+ini_set('allow_url_include', 0);
